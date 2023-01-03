10b62ef99a3bf070af94aff86c6dfe98550c399e
klaidas1591 <klaiduxlt@gmail.com>
2023-01-03 06:34:45 +0200

    pabaigti visi responsive formos dizainai, mobiliai versijai pridetas funkcionuojantis 'burgeris', isskaidytas pilnas komponentas, kad butu galima pernaudoti lengviau, kiek imanoma aptvarkytas apskritai visas dizainas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './Home';
 import './/styles/style.css';
+import './/styles/itemContainer.css';
+import './/styles/suggestionsContainer.css';
+import './/styles/header.css';
+import FullItem from './components/FullItem';
 
 function App() {
   return (
@@ -9,6 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} /> 
+        <Route path="item" element={<FullItem />} /> 
     </Routes>
   </Router>
   );
--- src/components/FullItem.js
+++ src/components/FullItem.js
@@ -1,20 +1,18 @@
 import React from 'react';
-// import ItemContainer from './ItemContainer';
-// import Suggestions from './Suggestions';
 import ITEMINFO from '../utils/data.json'
-import SUGGESTIONS from '../utils/suggestions.json'
+import image from '../images/grey_img.png'
+import SuggestionsCont from './SuggestionsCont';
 
-const FullItem = () => {
+const LastItem = () => {
   return (
-    <div className="content">
-      {/* {ITEM CONTENT} */}
+    <div className='content'>
       {ITEMINFO.map(val => {
         return (
-          <div className="fullItem" key={val.id}>
-{console.log("val id: ", val.id)}
+          <div className='fullItem' key={val.id}>
             <div className='itemContainer'>
+  {console.log("val id: ", val.id)}
               <div className="imageContainer">
-                <img src="" alt="" />
+                <img src={image} alt="" />
               </div>
               <div className="itemDescription">
                 <h2 className='itemsName'>{val.item_name}</h2> 
@@ -22,45 +20,12 @@ const FullItem = () => {
                 <span className='additionalInfo'>{val.additional_info}</span>
               </div>
             </div>
-             {/* {ITEM CONTENT} */}
-
-             {/* {SUGGESTIONS LIST} */}
-            <div className='suggestionsContainer'>
-              <div className="searchBar">
-                <input type="text" 
-                  placeholder="     Placeholder..."  
-                  />
-                  <button>SUBMIT</button>
-              </div>
-              <ul className='suggestionsList'>
-                {SUGGESTIONS.map(value => {        
-                  return(
-                    <li className='listElement' key={value.id}>
-{console.log("value id: ", value.id)}
-                        <div className='left'>
-                          <div className='itemThumbnail'>
-                          <img src="" alt="" />
-                          </div>
-                        </div>
-                        <div className='right'>
-                          <h3 className='itemsName'>{value.suggestion_name}</h3>
-                          <p className='suggestionDescription'>{value.suggestion_description}</p>
-                        </div>
-                    </li>
-                    )
-                  })}
-              </ul>
-              {/* {SUGGESTIONS LIST} */}
-            </div>
+            <SuggestionsCont />
           </div>
         )
       })}
-      
     </div>
-        // <ItemContainer /> }        pradine viso <FullItem /> versija (su
-        //                   }    -   minimaliais dizainu pakitimais) kai buvo
-        // <Suggestions />   }        rasoma "hardcode" principu duomenys(dummy tekstai)
   );
 };
 
-export default FullItem;
+export default LastItem;
